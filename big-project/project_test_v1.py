d4bd8583c6aac7338bdc4a64847a18df207f3889
KaiiMenai <kyra.menai.hamilton:outlook.com>
2026-03-22 17:24:48 +0000

    adding in task class into the db model

--- big-project/project_test_v1.py
+++ big-project/project_test_v1.py
@@ -13,3 +13,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+# Creating basic task table - this will be the basis for the task management system. It includes fields for title, description, completion status, and creation timestamp. 
+# # I wanted to make sure to include as many different types of data as possible, without overcomplicating the table.
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    done = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+# Method to convert Task object to dictionary for JSON response
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "done": self.done,
+            "created_at": self.created_at.isoformat(),
+        }

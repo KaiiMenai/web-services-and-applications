203a892aa716f95693cefcf058e350bf67b05fe7
KaiiMenai <kyra.menai.hamilton:outlook.com>
2026-05-07 18:57:37 +0100

    adding short commentary to the top of the user and category DAO.py's

--- big-project/categoryDAO.py
+++ big-project/categoryDAO.py
@@ -1,3 +1,8 @@
+# categoryDAO.py 
+# This program provides the categoryDAO class for managing category data in a database. It includes methods for creating, reading, updating, and deleting category records. The class uses SQLite for simplicity and is designed to be used in a Flask application. 
+# The methods include error handling and ensure that database connections are properly managed.
+# author: Kyra Menai Hamilton
+
 import sqlite3
 import dbconfig as cfg
 from os import path
--- big-project/userDAO.py
+++ big-project/userDAO.py
@@ -1,5 +1,5 @@
 # userDAO.py 
-# This module provides the UserDAO class for managing user data in a database. It includes methods for creating, reading, updating, and deleting user records. The class uses SQLite for simplicity and is designed to be used in a Flask application. 
+# This program provides the userDAO class for managing user data in a database. It includes methods for creating, reading, updating, and deleting user records. The class uses SQLite for simplicity and is designed to be used in a Flask application. 
 # The methods include error handling and ensure that database connections are properly managed.
 # author: Kyra Menai Hamilton
 
